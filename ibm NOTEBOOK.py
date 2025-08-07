282a7e1cf051756485b11fce847ea7345a7b44ce
Victor Manuel Hernandez <victormanuelhd04@gmail.com>
2025-08-07 17:48:27 -0500

    Add files via upload

---
+++ ibm NOTEBOOK.py
@@ -0,0 +1,60 @@
+# %% [markdown]
+# # Data Science Tools and Ecosystem
+
+# %% [markdown]
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# %% [markdown]
+# In this notebook, we summarize some of the most important tools, languages, and libraries in the field of Data Science.  
+# We will also perform some simple arithmetic operations using R code.
+
+# %% [markdown]
+# Some of the popular languages that Data Scientists use are:
+# - Python  
+# - R  
+# - SQL  
+# - Julia  
+# - Scala 
+
+# %% [markdown]
+# Some of the commonly used libraries used by Data Scientists include:
+# - Pandas
+# - NumPy
+# - Matplotlib
+# - ggplot2
+# - TensorFlow
+
+# %% [markdown]
+# | Data Science Tools       |
+# |--------------------------|
+# | Jupyter Notebook         |
+# | RStudio                  |
+# | Apache Zeppelin          |
+
+# %% [markdown]
+# Below are a few examples of evaluating arithmetic expressions in Python
+
+# %%
+# This is a simple arithmetic expression to multiply then add integers
+(3 * 4) + 5
+
+# %%
+# This will convert 200 minutes to hours by dividing by 60
+200 / 60
+
+# %% [markdown]
+# **Objectives:**
+# 
+# - List popular languages for Data Science
+# - List commonly used libraries for Data Science
+# - Create a table of Data Science tools
+# - Show examples of evaluating arithmetic expressions
+# - Convert minutes to hours using code
+# 
+
+# %% [markdown]
+# ---
+# author: "Victor Manuel Hernandez David"
+# ---
+
+
